11e6033948011fe00956b2171a7124974c8b8cd2
Max Rauchman <Max.L.Rauchman@gmail.com>
2017-04-10 19:34:25 -0400

    Add like actions for making ajax requests

--- frontend/actions/like_actions.js
+++ frontend/actions/like_actions.js
@@ -12,3 +12,17 @@ export const removeLike = (like) => ({
   type: REMOVE_LIKE,
   likeId: like.id
 });
+
+export const createLike = (like) => {
+  return (dispatch) => {
+    return APIUtil.createLike(like)
+      .then((newLike) => dispatch(receiveLike(newLike)));
+  };
+};
+
+export const deleteLike = (likeId) => {
+  return (dispatch) => {
+    return APIUtil.deleteLike(likeId)
+      .then((like) => dispatch(removeLike(like)));
+  };
+};
