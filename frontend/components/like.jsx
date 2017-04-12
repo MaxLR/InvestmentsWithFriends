196542c0bf25ec36a930b24df280544567733b91
Max Rauchman <Max.L.Rauchman@gmail.com>
2017-04-11 22:44:16 -0400

    Create like component

---
+++ frontend/components/like.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createLike, deleteLike } from '../../actions/like_actions';
+
+class LikeItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
